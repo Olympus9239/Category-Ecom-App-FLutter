fd6b3c947d9f94a291c1913d4964177c889336d3
Olympus9239 <ad9187@srmist.edu.in>
2021-06-20 17:14:01 +0530

    Day13(Local json)

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,9 +1,33 @@
+import 'dart:convert';
+
 import 'package:category_app/Widgets/drawer.dart';
 import 'package:category_app/Widgets/item_widgets.dart';
 import 'package:category_app/models/catalog.dart';
 import 'package:flutter/material.dart';
-class CategoryApp extends StatelessWidget {
+import 'package:flutter/services.dart';
+class CategoryApp extends StatefulWidget {
   const CategoryApp({Key key}) : super(key: key);
+
+  @override
+  _CategoryAppState createState() => _CategoryAppState();
+}
+
+class _CategoryAppState extends State<CategoryApp> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+       loadData()async{
+    final catalogJson=  await rootBundle.loadString("assets/catalog.json");
+      // print(catalogJson);
+      final decodeData =  jsonDecode(catalogJson);
+     // print(decodeData);
+     var  productData=decodeData["prodcuts"];
+     print(productData);
+
+       }    
+
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +38,7 @@ class CategoryApp extends StatelessWidget {
       ),
           body:Padding(
             padding: const EdgeInsets.all(16.0),
-            child: ListView.builder(          itemCount: CatalogModel.items.length,
+            child: ListView.builder( itemCount: CatalogModel.items.length,
 
               itemBuilder: (BuildContext context, int index){
                            return    ItemWidget(item: CatalogModel.items[index],);
